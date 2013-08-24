abb263351e66beca005e754f6f61b6bd6ac0ff0d
wtarr <william.projects.2012@gmail.com>
2013-08-24 16:32:28 +0100

    Added a ball with bounce material, created a blocker that targets player and when hit causes a camera shake.

---
+++ 10SecondBerserker/Assets/Scripts/Blocker.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using System.Collections;
+
+public class Blocker : MonoBehaviour {
+    
+    private float Walkspeed = 20.0f;
+    public GameObject TargetGameObject;
+    private bool reverse = false;
+    private float countdown = 0.5f;
+    
+
+    // Use this for initialization
+	void Start ()
+	{
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(TargetGameObject.transform);
+
+	    if (reverse)
+	    {
+	        countdown -= Time.deltaTime;
+            rigidbody.AddForce(transform.forward * Walkspeed * -0.5f);
+
+	        if (countdown <= 0)
+	        {
+	            reverse = false;
+	            countdown = 2;
+	        }
+
+	    }
+	    else
+	    {
+            rigidbody.AddForce(transform.forward * Walkspeed);    
+	    }
+        
+        
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Player"))
+            reverse = true;
+    }
+}
---
+++ 10SecondBerserker/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    private GameObject _camera;
+    private bool shake = true;
+    private const float Walkspeed = 20.0f;
+    private const float RotateSpeed = 5f;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    _camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.Space))// && shake == true)
+        //{
+        //    //shake = false;
+        //    _camera.SendMessage("CameraShake", new Vector2(1f, 0.5f));
+        //}
+
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        rigidbody.AddForce(transform.forward * Walkspeed);
+	    }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(transform.forward * -Walkspeed);
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        transform.Rotate(transform.up, -RotateSpeed);
+	    }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.up, RotateSpeed);
+        }
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Blocker"))
+            _camera.SendMessage("CameraShake", new Vector2(1f, 0.5f));
+    }
+}
